d2ef968f13f5df76f857fe1bc2a4899e572be765
CK151 <andersonoki15@gmail.com>
2022-12-12 15:25:54 +0300

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+const login = document.querySelector(".login-link"),
+ register = document.querySelector(".register-link"),
+ loginSection = document.querySelector(".login"),
+ registerSection = document.querySelector(".register"),
+ forgotSection = document.querySelector(".forgot"),
+ close = document.querySelector(".close"),
+ forgot = document.querySelector(".forgot-link");
+
+
+register.addEventListener("click", () => {
+    loginSection.style.display = "none";
+    registerSection.style.display = "flex";
+})
+
+
+login.addEventListener("click", () => {
+    registerSection.style.display = "none";
+    loginSection.style.display = "flex";
+})
+
+forgot.addEventListener("click", () => {
+    loginSection.style.display = "none";
+    forgotSection.style.display = "flex";
+    
+})
+
+close.addEventListener("click", () => {
+    forgotSection.style.display = "none";
+    loginSection.style.display = "flex";
+    
+})
